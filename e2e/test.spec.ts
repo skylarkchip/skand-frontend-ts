fd647ea025d36509f0cc0cc60a8feb0e26f920e3
Mark Marasigan <kramlivingstone@gmail.com>
2023-06-26 13:18:02 +0800

    test(test.spec.ts): updated test.spec.ts

--- e2e/test.spec.ts
+++ e2e/test.spec.ts
@@ -1,12 +1,7 @@
 import { test, expect } from "@playwright/test";
 
-const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://skand-frontend-ts.vercel.app/"
-    : "http://localhost:3000";
-
 test("User logs in", async ({ page }) => {
-  await page.goto(`${BASE_URL}`);
+  await page.goto("/");
 
   await page.fill('input[name="email"]', "test@example.com");
 
@@ -14,9 +9,9 @@ test("User logs in", async ({ page }) => {
 
   await page.click('button[type="submit"]');
 
-  await page.waitForURL(`${BASE_URL}/todo`);
+  await page.waitForURL("/todo");
 
-  await expect(page).toHaveURL(`${BASE_URL}/todo`);
+  await expect(page).toHaveURL("/todo");
 
   await page.fill("#todo", "Sample Todo");
 
